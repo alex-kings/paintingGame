7df2491a3fbf16cb4b62b7275c23d17e99581f9b
alex-kings <alexandrekings1@gmail.com>
2023-04-14 18:51:58 +0100

    Make progress.

--- Game.js
+++ Game.js
@@ -7,6 +7,8 @@ const gb3 = document.getElementById("guess3");
 const gb4 = document.getElementById("guess4");
 const buttons = [gb1,gb2,gb3,gb4];
 const paintingContainer = document.getElementById("paintingContainer");
+const endRoundPanel = document.getElementById("endRoundPanel");
+const nextRoundButton = document.getElementById("nextRoundButton")
 
 export class Game {
     // Attributes
@@ -19,11 +21,15 @@ export class Game {
     constructor(numberRounds, category) {
         this.category = category;
         this.rounds = getRounds(category,numberRounds);
-        this.setupButtons(0);
-        // this.displayPainting();
+        this.setupButtons();
+        this.displayPainting();
 
-        document.getElementById("test").addEventListener("click",()=>{
-            this.displayEndRoundPanel();
+        nextRoundButton.addEventListener("click",()=>{
+            // Hide the end round panel.
+            endRoundPanel.style.display = "none";
+            // Start the next round.
+            this.displayPainting();
+            this.setupButtons();
         })
     }
 
@@ -45,31 +51,29 @@ export class Game {
 
     // Check whether the guess was correct
     attemptGuess(btnNumber) {
+        let success = false;
         if(this.rounds[this.currentRound].painting[0] == this.rounds[this.currentRound].painters[btnNumber]) {
             // Correct guess
             this.incrementScore();
+            success = true;
         }
-        else {
-            // Wrong guess
-            console.log("Wrong guess")
-        }
-        this.incrementRound();
+        this.incrementRound(success);
     }
 
-    incrementRound() {
+    incrementRound(success) {
         this.currentRound++;
-        this.displayEndRoundPanel();
+        this.displayEndRoundPanel(success);
     }
 
     // Display the panel at the end of a round
-    displayEndRoundPanel() {
-        // Add an element on top of the painting
-        let div = document.createElement("div");
-        div.classList.add("mask")
-        div.innerHTML = `
-        Hello
+    displayEndRoundPanel(success) {
+        endRoundPanel.style.display = "block";
+        document.getElementById("endPanel").innerText = success ? `
+        Success
+        `:
         `
-        paintingContainer.appendChild(div);
+        Fail
+        `;
     }
 
     incrementScore() {
